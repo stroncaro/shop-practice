385200c9d7274600df35b0ee8b03e9c27773c82c
Stéfano Troncaro <36011367+stroncaro@users.noreply.github.com>
2024-07-17 18:25:57 -0300

    Create App component structure
    Create dummy implementation for all the components that make up the
    page layout. Implemented all in App.tsx for ease of use, refactor
    to separate files once everything is done.

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,103 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { PropsWithChildren } from "react";
+
+
+interface IClickable {
+  onClick?: () => void;
+}
+
+const IconButton: React.FC<PropsWithChildren<IClickable>> = ({ children, onClick }) => {
+  return (
+    <button onClick={onClick}>
+      {children}
+    </button>
+  )
+}
+
+const Sidebar: React.FC<PropsWithChildren> = ({ children }) => {
+  return (
+    <div>
+      {/* TODO: Add close button */}
+      {children}
+    </div>
+  )
+}
+
+const SidebarSelect: React.FC<PropsWithChildren> = ({ children }) => {
+  // TODO: Option elements passed as children? Or pass it a list of options and render in element?
+  // TODO: Set up functionality for user selecting a value
+  // TODO: Set up state to be expanded if value is selected
+  return (
+    <select>
+      {children}
+    </select>
+  )
+}
+
+const SearchBox: React.FC = () => {
+  // TODO: set state for search query
+  // TODO: validate input while user types
+  // TODO: filter cards
+  return (
+    <input type="search" />
+  )
+}
+
+const SortSelect: React.FC = () => {
+  // TODO: Set state and default behaviour
+  // TODO: Set options
+  // TODO: Sort cards according to option selected
+  return (
+    <select></select>
+  )
+}
+
+const CardGallery: React.FC<PropsWithChildren> = ({ children }) => {
+  return (
+    <div>
+      {children}
+    </div>
+  )
+}
+
+const Card: React.FC = () => {
+  return (
+    // TODO: add props with img, title, description, price and fav
+    <div>
+      <p>I'm a product</p>
+    </div>
+  )
+}
+
+const TextButton: React.FC<PropsWithChildren<IClickable>> = ({ children, onClick }) => {
+  return (
+    <button onClick={onClick}>
+      {children}
+    </button>
+  )
+}
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header>
+        <IconButton>Togle Menu Visibility Button</IconButton>
+        <h1>Find what you need</h1>
+        <IconButton>Sort Options</IconButton>
+        <SearchBox />
+        {/* TODO: Desktop */}
+        {/* <SortSelect /> */}
+      </header>
+      <Sidebar>
+        {/* SidebarSelect elements with product category options go here */}
+      </Sidebar>
+      <main>
+        <CardGallery>
+          {/* Fill with cards */}
+          <Card />
+        </CardGallery>
+        <TextButton>LoadMore</TextButton>
+      </main>
     </>
   )
 }
