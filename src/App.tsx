09ff8bc139c40561191192bebfd4ea17af0d4691
Stéfano Troncaro <36011367+stroncaro@users.noreply.github.com>
2024-07-17 20:49:02 -0300

    Add sidebar submenu to open up when a menu is clicked
    Sidebar menues were set up as <select> element, but that will pose
    problems for different rendering on mobile and desktop.
    Current implementation should meet the website design demands.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { PropsWithChildren } from "react";
+import { PropsWithChildren, useState } from "react";
 import { BsList, BsArrowDownUp, BsXCircle, BsHeart, BsHeartFill } from "react-icons/bs";
 
 interface IClickable {
@@ -13,26 +13,63 @@ const IconButton: React.FC<PropsWithChildren<IClickable>> = ({ children, onClick
   )
 }
 
-const Sidebar: React.FC<PropsWithChildren> = ({ children }) => {
+const Sidebar: React.FC = () => {
+  
+  // TODO: clean type defs. Should MenuItem be an enum?
+  type MenuItem = 'New In' | 'Clothing' | 'Footwear' | 'Accesories' | 'SALE';
+  type SubMenuItem = {
+    item: string;
+    action: null; // TODO: decide how to implement the action 
+  }
+
+  const menus: MenuItem[] = ['New In', 'Clothing', 'Footwear', 'Accesories', 'SALE'];
+  const subMenues: Partial<Record<MenuItem, SubMenuItem[]>> = {
+    'Clothing': [
+      { item: 'New In', action: null },
+      { item: 'See all', action: null },
+      { item: 'Coats', action: null },
+      { item: 'Beach clothes', action: null },
+      { item: 'Sweaters & hoodies', action: null },
+      { item: 'Shirts', action: null },
+      { item: 'Jeans and pants', action: null },
+      { item: 'T-shirts', action: null },
+      { item: 'Underwear', action: null },
+      { item: 'SALE', action: null },
+    ]
+  }
+
+  const [selectedMenuItem, setSelectedMenuItem] = useState<MenuItem | null>(null);
+
+  function setOrUnsetMenuItem(menuItem: MenuItem) {
+    const newValue = menuItem === selectedMenuItem ? null : menuItem;
+    setSelectedMenuItem(newValue);
+  }
+
   return (
     <div>
       <IconButton>
         <BsXCircle />
       </IconButton>
 
-      {children}
+      <ul id='sidebar-menu'>
+        {menus.map( (menuItem) =>
+          <li key={menuItem} onClick={() => setOrUnsetMenuItem(menuItem)}>
+            {menuItem}
+          </li>
+        )}
+      </ul>
+      
+      {selectedMenuItem && subMenues[selectedMenuItem] && (
+        <ul id='sidebar-submenu'>
+          {subMenues[selectedMenuItem].map( (subMenuItem) =>
+            // TODO: onClick
+            <li key={subMenuItem.item}>
+              {subMenuItem.item}
+            </li>
+          )}
+        </ul>
+      )}
     </div>
-  )
-}
-
-const SidebarSelect: React.FC<PropsWithChildren> = ({ children }) => {
-  // TODO: Option elements passed as children? Or pass it a list of options and render in element?
-  // TODO: Set up functionality for user selecting a value
-  // TODO: Set up state to be expanded if value is selected
-  return (
-    <select>
-      {children}
-    </select>
   )
 }
 
@@ -98,9 +135,7 @@ function App() {
         {/* TODO: Desktop */}
         {/* <SortSelect /> */}
       </header>
-      <Sidebar>
-        {/* SidebarSelect elements with product category options go here */}
-      </Sidebar>
+      <Sidebar />
       <main>
         <CardGallery>
           {/* Fill with cards */}
