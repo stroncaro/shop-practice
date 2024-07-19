b752631e9b38d736a1039662d708d2ed3250a308
Stéfano Troncaro <36011367+stroncaro@users.noreply.github.com>
2024-07-19 15:44:23 -0300

    Polish types

--- src/common/types.tsx
+++ src/common/types.tsx
@@ -6,17 +6,21 @@ export interface IProduct {
   price: number;
 }
 
-export type Tag = "clothing" | "corsets" | "dresses" | "girls' dresses" | "gowns" | "men's shirts" | "men's t-shirts" | "skirts" | "suits";
-
-export type Sort = "none" | "name" | "price";
-export type SortFunctionArray = {
-  [key in Sort]: ((a: IProduct, b: IProduct) => number) | null;
-};export type MenuItem = 'New In' | 'Clothing' | 'Footwear' | 'Accesories' | 'SALE';
-export type SubMenuItem = {
-  item: string;
-  tag: Tag | null;
-};
 export interface IClickable {
   onClick?: () => void;
 }
 
+export type Tag = "clothing" | "corsets" | "dresses" | "girls' dresses" | "gowns" | "men's shirts" | "men's t-shirts" | "skirts" | "suits";
+
+export type Sort = "none" | "name" | "price";
+
+export type SortFunctionArray = {
+  [key in Sort]: ((a: IProduct, b: IProduct) => number) | null;
+};
+
+export type MenuItem = 'New In' | 'Clothing' | 'Footwear' | 'Accesories' | 'SALE';  
+
+export type SubMenuItem = {
+  item: string;
+  tag: Tag | null;
+};  
