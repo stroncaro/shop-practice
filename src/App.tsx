0d1f8272a9c9fca90819ea247f57038642ec8fc3
Stéfano Troncaro <36011367+stroncaro@users.noreply.github.com>
2024-07-19 09:11:36 -0300

    Add product sorting

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { PropsWithChildren, useCallback, useEffect, useState } from "react";
+import { ChangeEvent, PropsWithChildren, useCallback, useEffect, useState } from "react";
 import { BsList, BsArrowDownUp, BsXCircle, BsHeart, BsHeartFill } from "react-icons/bs";
 
 interface IClickable {
@@ -88,12 +88,21 @@ const SearchBox: React.FC<SearchBoxProps> = ({ onInput }) => {
   )
 }
 
-const SortSelect: React.FC = () => {
-  // TODO: Set state and default behaviour
-  // TODO: Set options
-  // TODO: Sort cards according to option selected
+interface SortSelectProps {
+  value: Sort;
+  onChange: (event: ChangeEvent<HTMLSelectElement>) => void;
+  options: { [key in Sort]: string }
+}
+
+const SortSelect: React.FC<SortSelectProps> = ({ value, onChange, options }) => {
   return (
-    <select></select>
+    <select id="sort" value={value} onChange={onChange}>
+      {Object.entries(options).map(([key, value]) =>
+        <option key={key} value={key}>
+          {value}
+        </option> 
+      )}
+    </select>
   )
 }
 
@@ -139,6 +148,10 @@ const TextButton: React.FC<PropsWithChildren<IClickable>> = ({ children, onClick
 
 type Tag = "clothing" | "corsets" | "dresses" | "girls' dresses" | "gowns" | "men's shirts" | "men's t-shirts" | "skirts" | "suits";
 
+type Sort = "none" | "name" | "price";
+type SortFunctionArray = { [key in Sort]: ((a: IProduct, b: IProduct) => number ) | null }
+
+
 function App() {
   /* UI state */
   const [showSidebar, setShowSidebar] = useState<boolean>(false);
@@ -149,10 +162,28 @@ function App() {
   const [cache, setCache] = useState<IProduct[]>([]);
   const [tag, setTag] = useState<Tag | null>(null);
 
+  // TODO: decouple sorting from fetching products
+  const [sort, setSort] = useState<Sort>("none");
+  const SORT_FUNCTIONS: SortFunctionArray = {
+    none: null,
+    price: (a, b) => a.price - b.price,
+    name: (a, b) => {
+      if (a.title > b.title) {
+        return 1;
+      }
+  
+      if (a.title < b.title) {
+        return -1;
+      }
+  
+      return 0;
+    }
+  }
+  
   const MINIMUM_QUERY_LENGTH = 3;
   const QUERY_UPDATE_DELAY_MS = 500;
   const [query, setQuery] = useState<string>('');
-
+  
   let queryTimeout: number;
   const debounceQuery: (val: string) => void = (val) => {
     clearTimeout(queryTimeout);
@@ -184,7 +215,9 @@ function App() {
     const limit = PRODUCTS_PER_FETCH;
     const skip = overwrite ? 0 : products.length;
     const shouldCache = !shouldSearch && cache.length < limit + skip;
-  
+    const sortFunction = SORT_FUNCTIONS[sort];
+    const shouldSort = !!sortFunction;
+
     const url = 'https://dummyjson.com/products'
       .concat(shouldSearch ? `/search?q=${query}&` : '?')
       .concat(`limit=${limit}&`)
@@ -197,6 +230,7 @@ function App() {
         setProducts((prev) => {
           const fetchedValues = data.products;
           const updatedValues = overwrite ? fetchedValues : [...prev, ...fetchedValues];
+          if (shouldSort) updatedValues.sort(sortFunction);
           if (shouldCache) setCache(updatedValues);
           return updatedValues;
         });
@@ -205,7 +239,7 @@ function App() {
         // TODO: do something meaningful with the error
         console.log(error);
       });
-  }, [products, query]);
+  }, [products, query, sort]);
 
   /* TODO: It appears to trigger twice in dev because of React Strict Mode mounting
   and remounting the component. Check that in production this only happens one time */
@@ -222,8 +256,16 @@ function App() {
           <BsArrowDownUp />
         </IconButton>
         <SearchBox onInput={(e) => debounceQuery((e.target as HTMLInputElement).value)}/>
-        {/* TODO: Desktop */}
-        {/* <SortSelect /> */}
+        {/* TODO: Visible in desktop only */}
+        <SortSelect
+          value={sort}
+          onChange={(e) => setSort(e.target.value as Sort)}
+          options={{
+            none: "Sort by",
+            name: "Name",
+            price: "Price",
+          }}
+        />
       </header>
       {showSidebar && <Sidebar
         closeSidebarProc={() => setShowSidebar(false)}
